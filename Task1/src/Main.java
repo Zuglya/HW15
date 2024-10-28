5fcd021b479221eb879e411a7a478de62be85563
Zuglya <irussel86@gmail.com>
2024-10-28 20:56:22 +0400

    Решение ДЗ Урока 15

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    public static final String RUSSIAN_CHARS= "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     public static void main(String[] args) {
         //todo код писать тут
+        for(int i = 0; i < RUSSIAN_CHARS.length(); i++) {
+            System.out.printf("%s-%d\n", RUSSIAN_CHARS.charAt(i), (int)RUSSIAN_CHARS.charAt(i));
+        }
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String fio = new Scanner(System.in).nextLine().trim();
+
+        convertFio(fio);
+
+
+        }
+
+
+    private static void convertFio(String fio) {
+        final String CORRECT_CHARS = " абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-";
+        boolean isValid = true;
+        int spaceCount = 0;
+
+        for(int i = 0; i < fio.length(); i++) {
+            if(fio.charAt(i) == ' ') {
+                spaceCount++;
+            }
+            if(!CORRECT_CHARS.contains(String.valueOf(fio.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if(isValid && spaceCount == 2) {
+            System.out.printf("Фамилия: %s\nИмя: %s\nОтчество: %s", fio.substring(0, fio.indexOf(' ')),
+                    fio.substring(fio.indexOf(' '), fio.lastIndexOf(' ')), fio.substring(fio.lastIndexOf(' ')));
+        } else {
+            System.out.println("Ввод не корректен");
+        }
+
     }
 }
